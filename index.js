d6dfc37ab6b89e605c6d1bbb51cbedfb498b2858
IanNdiba <ianndiba@gmail.com>
2020-02-05 14:09:39 +0300

    using prompt package

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 console.log("Test Linking VS Code to Github");
 
-var input = prompt("Enter Your Name: ");
-alert("Your name is: " + input);
+var prompt = require('prompt');
+
+//starting the prompt
+prompt.start();
+
+prompt.get(['firstName' , 'lastName'], function(err, result){
+
+    //alert method did not work
+    //using prompt package
+    console.log("User Input Received: ");
+    console.log("First Name: " + result.firstName);
+    console.log("Last Name: " + result.lastName);
+});
+
+
+
